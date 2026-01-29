f073a743da8035f9fd08c5f3d9b1574b42317b2e
haris-187 <theharis18@gmail.com>
2026-01-30 03:07:58 +0500

    Refactor move button functionality to use moveRandomEl

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const moveRandom = document.querySelector("#move-random");
-
-function moveButton() {
-  const windowWidth = window.innerWidth - moveRandom.offsetWidth;
-  const windowHeight = window.innerHeight - moveRandom.offsetHeight;
-
-  const randomX = Math.floor(Math.random() * windowWidth);
-  const randomY = Math.floor(Math.random() * windowHeight);
-
-  moveRandom.style.position = "absolute";
-  moveRandom.style.left = randomX + "px";
-  moveRandom.style.top = randomY + "px";
+function moveRandomEl(elm) {
+  elm.style.position = "absolute";
+  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
 }
 
-moveRandom.addEventListener("mouseenter", moveButton);
-moveRandom.addEventListener("touchstart", moveButton);
+const moveRandom = document.querySelector("#move-random");
+
+moveRandom.addEventListener("mouseenter", function (e) {
+  moveRandomEl(e.target);
+});
+
